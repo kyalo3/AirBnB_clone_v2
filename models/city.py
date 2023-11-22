7e56b8836776d14b602924f866224427a66fe386
Mally13 <mallymutuku13@gmail.com>
2023-11-23 01:33:07 +0300

    Task 8: DBStorage Place

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from os import getenv
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -11,3 +11,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete-orphan")    
--- models/place.py
+++ models/place.py
@@ -11,7 +11,7 @@ class Place(BaseModel, Base):
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(60), nullable=False)
+    name = Column(String(128), nullable=False)
     description = Column(String(1024),nullable=True )
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False) 
@@ -19,12 +19,9 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    reviews = relationship('Review', cascade='all, delete-orphan', backref='place')
 
     @property
     def reviews(self):
         """Getter attribute that returns the list of Review instances"""
         return (
             [review for review in self.reviews if review.place_id == self.id])
-
-    amenity_ids = []
