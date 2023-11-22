570d725fdf452f97b2cdd00f83ab3719293ee37c
Mally13 <mallymutuku13@gmail.com>
2023-11-22 23:53:46 +0300

    Task 5: Fix missing tablename

--- models/state.py
+++ models/state.py
@@ -8,12 +8,10 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
-    else:
-        name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
 
     @property
     def cities(self):
--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,16 +11,9 @@ Base = declarative_base()
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', cascade='all, delete-orphan', backref='user')
-        reviews = relationship('Review', cascade='all, delete-orphan', backref='user')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
