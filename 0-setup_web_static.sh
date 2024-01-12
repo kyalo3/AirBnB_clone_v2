35a31747e533ee4dd70072009e9d3e8bea6f3391
Mally13 <mallymutuku13@gmail.com>
2024-01-12 17:32:43 +0300

    Task 0:Depoy Static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,12 +10,11 @@ sudo mkdir -p /data/web_static/shared/
 
 sudo touch /data/web_static/releases/test/index.html
 sudo sh -c 'echo "<html>
-	<head>
-		<title>Welcome to Airbnb!</title>
-	</head>
-	<body>
-		<h1>Success! The airbnb server block is working!</h1>
-	</body>
+  <head>
+  </head>
+  <body>
+	Holberton School
+  </body>
 </html>" > /data/web_static/releases/test/index.html'
 
 if [ -L /data/web_static/current ]; then
@@ -34,8 +33,8 @@ server {
 
 	root /var/www/html;
 
-	location /hbnb_static {
-		alias /data/web_static/current;
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
 		index index.html index.htm;
 	}
 	error_page 404 /404.html;
