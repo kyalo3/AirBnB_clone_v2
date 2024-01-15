fb486331dff5c772144ce42dbd0d63562e7e59a3
Mally13 <mallymutuku13@gmail.com>
2024-01-15 19:19:00 +0300

    Task 0:Depoy Static files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
-# Setup Nginx to serve Airbnb static files
+# Setup Nginx to serve airbnb static files
 
 if ! command -v nginx &> /dev/null; then
 	sudo apt-get update
 	sudo apt-get install -y nginx
 fi
-
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-sudo tee /data/web_static/releases/test/index.html <<EOF
-<html>
-  <head>
-  </head>
-  <body>
-	Holberton School
-  </body>
-</html>
-EOF
+sudo touch /data/web_static/releases/test/index.html
+sudo sh -c 'echo "<html>
+	<head>
+		<title>Welcome to Airbnb!</title>
+	</head>
+	<body>
+		<h1>Success! The airbnb server block is working!</h1>
+	</body>
+</html>" > /data/web_static/releases/test/index.html'
 
 if [ -L /data/web_static/current ]; then
-	sudo rm /data/web_static/current
+	rm /data/web_static/current
 fi
 
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
@@ -35,25 +34,23 @@ server {
 
 	root /var/www/html;
 
-	location /hbnb_static/ {
-	    alias /data/web_static/current/;
-	    index index.html index.htm;
+	location /hbnb_static {
+		alias /data/web_static/current;
+		index index.html index.htm;
 	}
-
 	error_page 404 /404.html;
 	location = /404.html {
-	    root /usr/share/nginx/html;
-	    internal;
+		root /usr/share/nginx/html;
+		internal;
 	}
-
 	location / {
-	    try_files $uri $uri/ =404;
+		try_files $uri $uri/ =404;
 	}
 }
 EOF
 
 # Create a symbolic link to enable the server block
-sudo ln -sf /etc/nginx/sites-available/airbnb /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/airbnb /etc/nginx/sites-enabled/
 
 # Restart Nginx
 sudo service nginx restart
