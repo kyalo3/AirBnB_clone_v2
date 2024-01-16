379bbcbe599740dc26dd47cc98886e6c23453259
Mally13 <mallymutuku13@gmail.com>
2024-01-16 19:35:24 +0300

    Task 2

---
+++ 1-pack_web_static.py
@@ -0,0 +1,9 @@
+from fabric.api import local
+from datetime import datetime
+"""A fabric script that compresses contents of webstatic folder"""
+
+
+def do_pack():
+	"""Generates a .tgz archive"""
+	zip_name=f"web_static_{current_datetime.strftime('%Y%m%d%H%M%S')}.tgz"
+	local("tar -czvf {zip_name} webstatic")
