19740a21e422e2924019a8e5cc55d8b04a5b9a53
Mally13 <mallymutuku13@gmail.com>
2024-01-17 15:34:01 +0300

    Task 1: Fab file to archive a folder

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,13 +2,16 @@
 from fabric.api import local
 from datetime import datetime
 import os
-"""A fabric script that compresses contents of webstatic folder"""
+"""
+A fabric script that compresses contents of webstatic folder
+"""
 
 
 def do_pack():
-	"""Generates a .tgz archive"""
+	"""
+	Generates a .tgz archive
+	"""
 	try:
-	    ""
 		local("mkdir -p versions")
 		zip_name=f"versions/web_static_{current_datetime.strftime('%Y%m%d%H%M%S')}.tgz"
 		local(f"tar -czvf {zip_name} webstatic")
