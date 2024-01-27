841e679b19b8a151af4873e6560e5e2ae8d8e964
Kyalo3 <kyalokimeu0@gmail.com>
2024-01-27 09:46:13 +0300

    flask app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-
+""" model- flask app"""
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """string hello hbnb"""
     return "Hello HBNB!"
 
 
