4487c1ddba0895e72e7363c92bd4d66a10b0b79b
Kyalo3 <kyalokimeu0@gmail.com>
2024-01-27 09:58:16 +0300

    Write a script that starts a Flask web application:

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def hello_hbnb():
     """string hello hbnb"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ an empty function that returns the string hbnb"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
